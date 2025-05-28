693d8f53784e07b36873c6738d11b6fd4b48b16a
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-05-27 20:08:49 -0600

    feat: add capability to resize grid size

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 let container = document.querySelector(".container");
 
-let gridSize = 30;
-container.style.width = `${gridSize * 10}px`;
+// canvas width in px
+const canvasWidth = 500;
+container.style.width = `${canvasWidth}px`;
+container.style.height = `${canvasWidth}px`;
 
-for(let i = 0; i < gridSize; i++){
-    for(let j = 0; j < gridSize; j++){
-        let square = document.createElement("div");
-        square.className = "square";
-        container.appendChild(square);
+
+function createGrid(gridSize, canvasWidth){
+    // square width in px
+    let squareWidth =  canvasWidth / gridSize;
+
+    // clear canvas
+    container.innerHTML = '';
+
+    // create grid
+    for(let i = 0; i < gridSize; i++){
+        for(let j = 0; j < gridSize; j++){
+            let square = document.createElement("div");
+            square.className = "square";
+            square.style.width = `${squareWidth}px`;
+            square.style.height = `${squareWidth}px`;
+            container.appendChild(square);
+        }
     }
+
 }
+
+// initialize
+createGrid(40, canvasWidth);
+
 
 container.addEventListener('mouseover', (event) => {
     let target = event.target;
-
     target.classList.add('coloreado');
+
 })
+
+const resizeBtn = document.querySelector('#resizeBtn');
+
+function resizeAndResetGrid(){
+    let gridSize = prompt("Set grid size (1, 100):");
+    if (!Number.isInteger(+gridSize) || gridSize < 1 || gridSize > 100 ) {
+        alert("Invalid value, pleas enter an integer between 1 and 100")
+    } else {
+        createGrid(gridSize, canvasWidth);
+    }
+}
+
+resizeBtn.addEventListener('click', resizeAndResetGrid);
