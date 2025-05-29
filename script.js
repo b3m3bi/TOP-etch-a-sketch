5e10b48c7df5cbff6f54d1302e183ad2819a9747
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-05-29 14:05:58 -0600

    feat: add multiple colors and eraser

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let container = document.querySelector(".container");
 
+const colorsRGB = {
+    black: [0,0,0],
+    red: [246,0,0],
+    orange: [255,140,0],
+    yellow: [255,238,0],
+    green: [77,233,76], 
+    blue: [55,131,255], 
+    violet: [72,21,170]
+}
+
+
 // canvas width in px
-const canvasWidth = 800;
+const canvasWidth = 500;
 container.style.width = `${canvasWidth}px`;
 container.style.height = `${canvasWidth}px`;
 
@@ -14,58 +25,79 @@ function createGrid(gridSize, canvasWidth){
     container.innerHTML = '';
 
     // create grid
-    for(let i = 0; i < gridSize; i++){
-        for(let j = 0; j < gridSize; j++){
-            let square = document.createElement("div");
-            square.className = "square";
-            square.style.width = `${squareWidth}px`;
-            // square.style.height = `${squareWidth}px`;
-            container.appendChild(square);
-        }
+    for(let i = 0; i < gridSize * gridSize; i++){
+        let square = document.createElement('div');
+        square.className = 'square';
+        square.style.flexBasis = `${100/gridSize}%`;
+        container.appendChild(square);
     }
 
 }
 
-// initialize
-createGrid(40, canvasWidth);
-
 function randomNumber(num){
-    return Math.floor(Math.random() * (num + 1));
+    return Math.floor(Math.random() * num);
+}
+
+function randomElementFromArray(arr){
+    let randomIndex = randomNumber(arr.length);
+    return arr[randomIndex];
 }
 
 
-let randomColorsCheckbox = document.querySelector('#random_colors');
+let rainbowCheckbox = document.querySelector('#rainbow');
 let opacityCheckbox = document.querySelector('#opacity');
+let eraserCheckbox = document.querySelector('#eraser');
 
 container.addEventListener('mouseover', (event) => {
     let target = event.target;
-    let opacityValue = opacityCheckbox.checked ? 0.1 : 1;
 
-    if (!target.classList.contains('coloreado')){
-        target.classList.add('coloreado');
-        if (randomColorsCheckbox.checked){
-            target.style.backgroundColor = `rgba(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)}, ${opacityValue})`;
-        } else {
-            target.style.backgroundColor = `rgba(0, 0, 0, ${opacityValue})`;
-        }
+    if (eraserCheckbox.checked){
+        // si está activado el borrador, borrar
+        target.style.backgroundColor = '';
+        target.className = 'square';
     } else {
-        let rgba = target.style.backgroundColor.slice(5, -1).split(',').map(elem => +elem);
-        if (rgba[3] < 1){
-            target.style.backgroundColor = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${rgba[3] + 0.1})`;
-        }
-    }
+        // si está descactivado el borrador, pintar...
+        let rainbowColors = ['red','orange','yellow','green','blue','violet']
+        let color = rainbowCheckbox.checked ? randomElementFromArray(rainbowColors) : document.querySelector('input[name="color"]:checked').id;
+        let colorRGB = colorsRGB[color]
 
+        if (!target.classList.contains(color)){
+            let opacityValue = opacityCheckbox.checked ? 0.1 : 1;
+            target.style.backgroundColor = `rgba(${colorRGB[0]}, ${colorRGB[1]}, ${colorRGB[2]}, ${opacityValue})`;
+            target.className = `square ${color}`;
+        } else {
+            let rgba = target.style.backgroundColor.slice(5, -1).split(',').map(elem => +elem);
+            if (rgba[3] < 1){
+                target.style.backgroundColor = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${rgba[3] + 0.1})`;
+            }
+        }
+
+    }
 })
 
+
 const resizeBtn = document.querySelector('#resizeBtn');
+let canvasSize = document.querySelector('#canvas-size');
+const canvasSizeDisplay = document.querySelector('#canvas-size-value');
+canvasSizeDisplay.innerHTML = canvasSize.value;
 
-function resizeAndResetGrid(){
-    let gridSize = prompt("Set grid size (1, 100):");
-    if (!Number.isInteger(+gridSize) || gridSize < 1 || gridSize > 100 ) {
-        alert("Invalid value, pleas enter an integer between 1 and 100")
-    } else {
-        createGrid(gridSize, canvasWidth);
-    }
-}
+canvasSize.addEventListener('input', () => {
+    canvasSizeDisplay.innerHTML = canvasSize.value;
+})
 
-resizeBtn.addEventListener('click', resizeAndResetGrid);
+canvasSize.addEventListener('change', () => {
+    createGrid(canvasSize.value, canvasWidth);
+})
+
+
+// resizeBtn.addEventListener('click', resizeAndResetGrid);
+
+const eraseBtn = document.querySelector('#erase-all');
+
+eraseBtn.addEventListener('click', () => {
+    createGrid(canvasSize.value, canvasWidth);
+})
+
+
+// initialize
+createGrid(canvasSize.value, canvasWidth);
